a5bac8ffe4af018f2b87b56b807123839501803e
eonist <30n1st@gmail.com>
2021-06-05 13:53:28 +0200

    Compile to 10.15 only

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SVGWrapper",
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(
             name: "SVGWrapper",
