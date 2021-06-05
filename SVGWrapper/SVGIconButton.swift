1994aa73f8d8f582fd04eabf9fce1ad196f1e3e6
eonist <30n1st@gmail.com>
2021-06-05 16:57:07 +0200

    Begin fixing resize issue with SwiftSVG

--- SVGWrapper/SVGIconButton.swift
+++ SVGWrapper/SVGIconButton.swift
@@ -30,13 +30,43 @@ extension SVGIconButton {
     * SVGGraphic
     */
    func createSVGGraphic() -> UIView {
-      let svgURL = URL(string: "https://openclipart.org/download/181651/manhammock.svg")!
-      let svgGraphic = UIView(SVGURL: svgURL) { (svgLayer) in
+      let svgURLStr: String = Bundle.main.resourcePath! + "/Assets.bundle/apple.svg"
+//      _ = svgURLStr
+      guard let svgURL = URL(string: svgURLStr) else { fatalError("⚠️️ unable to create URL from: \(svgURLStr)") }// URL(string: "https://openclipart.org/download/181651/manhammock.svg")!
+//      Swift.print(Self.content(Bundle.main.resourcePath! + "/Assets.bundle/shopify.svg"))//Output: testing✌️
+      Swift.print("svgURL:  \(svgURL)")
+      Swift.print("svgURL.absoluteString:  \(svgURL.absoluteString)")
+      // try absolute URL, try passing Data?
+      let content = try? String(contentsOfFile: svgURL.path, encoding: .utf8)
+      let data = Data(content!.utf8)
+      Swift.print("data.count:  \(data.count)")
+//      Swift.print("content:  \(content)")
+//      let svgData = try? Data(contentsOf: URL.init(string: svgURL.path)!)
+//      Swift.print("svgData.count:  \(svgData?.count)")
+      let svgGraphic = UIView.init(SVGData: data) { (svgLayer) in // UIView.init(SVGURL: svgURL)
+         Swift.print("svgLayer.boundingBox:  \(svgLayer.boundingBox)")
          svgLayer.fillColor = self.color.cgColor // UIColor(red:0.52, green:0.16, blue:0.32, alpha:1.00).cgColor
-         svgLayer.resizeToFit(.init(origin: .zero, size: self.iconSize)) // uses the smallest side
+//         svgLayer.resizeToFit(.init(origin: .zero, size: .init(width: 48, height: 48)/*self.iconSize*/)) // uses the smallest side
+         let offset: CGPoint = .init(x: -4, y: -32)
+         let toSize: CGSize = .init(width: 48, height: 48)
+//         let newWidth: CGFloat = svgLayer.boundingBox.width
+         let newSize: CGSize = {
+            if svgLayer.boundingBox.width > svgLayer.boundingBox.height { // landscape
+               let multiplier = toSize.width / svgLayer.boundingBox.width
+               return .init(width: toSize.width, height: svgLayer.boundingBox.height * multiplier)
+            } else { // portrait
+               let multiplier = toSize.height / svgLayer.boundingBox.height
+               return .init(width: svgLayer.boundingBox.width * multiplier, height: toSize.height)
+            }
+         }()
+         Swift.print("newSize:  \(newSize)")
       }
       self.addSubview(svgGraphic)
-      svgGraphic.anchorAndSize(to: self, width: self.iconSize.width, height: self.iconSize.height, align: .centerCenter, alignTo: .centerCenter, sizeOffset: .zero)
+//      svgGraphic.anchorAndSize(to: self, width: self.iconSize.width, height: self.iconSize.height, align: .centerCenter, alignTo: .centerCenter, sizeOffset: .zero)
       return svgGraphic
    }
 }
+// add .svg brand icon to project, and try to load it
+   // read up on .bundle 👈
+// add the same to desktopUI, make a dummy test view and try to load it
+// try to make the brand and icon btns etc in desktopui 👈
--- SVGWrapper/View.swift
+++ SVGWrapper/View.swift
@@ -4,13 +4,27 @@ import Spatial
 import With
 
 class View: UIView {
+   /**
+    * 
+    */
+   static func content(_ path: String) -> String? {
+      do {
+         let content = try String(contentsOfFile: path, encoding: .utf8) as String//encoding: NSUTF8StringEncoding
+         return content
+      } catch {
+         return nil
+      }
+   }
    override init(frame: CGRect) {
       super.init(frame: frame)
       let testView = with(SVGIconButton.init(iconSize: .init(width: 32, height: 32), color: .systemPurple)) {
          addSubview($0)
-         $0.anchorAndSize(to: self, width: 100, height: 42)
+         $0.anchorAndSize(to: self, width: 48, height: 48)
       }
       _ = testView
+//      Swift.print(Bundle.main.resourcePath!+"/Assets.bundle/shopify.svg")
+      
+      Swift.print(Self.content(Bundle.main.resourcePath! + "/Assets.bundle/shopify.svg"))//Output: testing✌️
 //      let cowboyHat = UIView(SVGNamed: "cowboyHat")     // In the asset catalog as a Data Asset
 //      self.addSubview(cowboyHat)
       // add graphic to view 👈
