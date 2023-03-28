035df673602338f09fc0a0dc133df4c816ba3261
eonist <30n1st@gmail.com>
2023-03-28 04:56:19 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SVGWrapper",
-    platforms: [.iOS(.v13), .macOS(.v10_15)], // update to ios15? 
+    platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         .library(
             name: "SVGWrapper",
